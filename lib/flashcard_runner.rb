9e74bff0e709217c77768402c7f10c401b1f16b7
themenintights <seth19483@Gmail.com>
2017-01-06 08:42:20 -0700

    Update flashcard_runner.rb

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,7 +3,7 @@ require'./lib/round'
 require'./lib/deck'
 
 card_1 = Card.new("Question: What is 5 + 5?", "10") 
-card_2 = Card.new("Question: What is Rachel's favorite animal?", "redpanda") 
+card_2 = Card.new("Question: What is Rachel's favorite animal?", "red panda") 
 card_3 = Card.new("Question: What is Mike's middle name?", "nobody knows") 
 card_4 = Card.new("Question: What cardboard cutout lives at Turing?", "justin bieber") 
 deck = Deck.new([card_1, card_2, card_3, card_4])
