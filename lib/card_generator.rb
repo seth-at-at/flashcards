2c08ac31e85abeb1cd3bff56fcb4fbb9ccd1bb68
themenintights <seth19483@Gmail.com>
2017-01-06 09:12:54 -0700

    Update card_generator.rb

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,21 @@
+require'./lib/card'
+require'./lib/deck'
+require'./lib/round'
+
 
 class CardGenerator
+  attr_reader :card_file, :array, :card
 
+  def initialize(file)
+    @card_file = file
+    @card = Array.new
+  end
+
+  def cards
+    File.readlines(card_file).map do |line|
+        @card << line.split(",")
+        line_num = line.split(",")
+        Card.new(line_num[0], line_num[1].chomp)
+       end
+  end
 end
